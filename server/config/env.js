db1103d6ffee7a10a9aa75d7a7f16da4269a2f22
muhammedArsat <ars.code21@gmail.com>
2025-07-27 20:45:45 +0530

    minor bug fixed

--- server/config/env.js
+++ server/config/env.js
@@ -1,5 +1,5 @@
 import { config } from "dotenv";
 
-config({ path: `.env.${process.env.NODE_ENV || "development"}.local` });
+config()
 export const { PORT, NODE_ENV, MONGODB_URI, JWT_SECRET, JWT_EXPIRES_IN,ARCJET_KEY,ARCJET_ENV,EMAIL_PASS,EMAIL_USER ,FRONTEND_URL} =
   process.env;
